df87116b68c9282b4e74a7fc7c1f562266ba212a
Kiran Chavan <kirank.chavan@gmail.com>
2022-12-12 14:57:26 -0500

    First Commit .....

---
+++ src/test/java/TestPackage/Test1.java
@@ -0,0 +1,17 @@
+package TestPackage;
+
+public class Test1 {
+	
+	
+	public static void main(String[] args) {
+		String Hungary ="yes";
+		
+		if(Hungary=="yes") {
+			System.out.println("eat something >>>>");
+		}
+		else {
+			System.out.println("do other work");
+		}
+		
+	}
+}
