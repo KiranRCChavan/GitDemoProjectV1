187a9723c3f81f68f7bdb210fae50d458d283933
KiranRCChavan <40853284+KiranRCChavan@users.noreply.github.com>
2022-12-13 13:14:03 -0500

    Update Test1.java

--- src/test/java/TestPackage/Test1.java
+++ src/test/java/TestPackage/Test1.java
@@ -6,11 +6,22 @@ public class Test1 {
 	public static void main(String[] args) {
 		String Hungary ="yes";
 		
-		if(Hungary=="yes") {
+		if(Hungary=="No") {
 			System.out.println("eat something >>>>");
 		}
 		
 		
+		else {
+			System.out.println("dgfghghfhjjhdo other work");
+		}
+		
+		else {
+			System.out.println("dgfghghfhjjhdo other work");
+		}
+		
+		else {
+			System.out.println("dgfghghfhjjhdo other work");
+		}
 		else {
 			System.out.println("dgfghghfhjjhdo other work");
 		}
